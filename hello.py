4ab333c236a41019b213e9178bcb26bc8b0f72d8
Kiranmayi <kiranmayibandaru@gmail.com>
2026-07-03 20:41:02 +0530

    deleted the old .pkl files

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
 import streamlit as st
+import pandas as pd
+import joblib
+
 st.write("app started successfuly")
+st.write("second line of page")
+model = joblib.load("xgb_model.pkl")
+print(type(model))
